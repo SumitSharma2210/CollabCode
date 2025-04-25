0c60036081ac707f412d7f6a64ac0262a70e27e1
SumitSharma2210 <sumit.sharma.gkv@gmail.com>
2025-04-25 23:53:13 +0530

    fifth commit

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -10,9 +10,7 @@ export default function LandingPage() {
       <header className="px-4 lg:px-6 h-14 flex items-center justify-between">
         <Link className="flex items-center justify-center" href="#">
           <Code2 className="h-6 w-6" />
-          <span className="ml-2 text-lg font-bold">
-            Faculty of Engineering & Technology
-          </span>
+          <span className="ml-2 text-lg font-bold">CollabCode</span>
         </Link>
         <div className="flex items-center gap-4">
           <ThemeToggle />
@@ -142,7 +140,7 @@ export default function LandingPage() {
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-gray-500 dark:text-gray-400">
-          Gurukula Kangri (Deemed to be University)
+          New features coming soon
         </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link
